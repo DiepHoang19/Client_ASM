cca1822f17d4c0e1c7f48d20510216b25a9bf6e7
DiepHoang19 <93962691+DiepHoang19@users.noreply.github.com>
2022-08-01 22:24:07 +0700

    first commit

---
+++ src/Modules/Public/Account/Account.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export class Account extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+    render() {
+        return (
+            <>
+            </>
+        )
+    }
+}
---
+++ src/Modules/Public/Shared/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export class Fôter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+    render() {
+        return (
+            <>
+            </>
+        )
+    }
+}
